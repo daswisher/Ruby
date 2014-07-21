35da130e061e7e378d089fe128c294709b383dc8
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-07-20 18:56:10 -0600

    Updating rubyApi reference material to include information about how to deal with XML

--- Reference Material/rubyApi.rb
+++ Reference Material/rubyApi.rb
@@ -1,4 +1,12 @@
 require 'open-uri' #This imports the modules for http interactions
+
+#It is possible to parse in xml with ruby using the following require line
+#require 'rexml/document'
+#To read in the xml file, we would use:
+#file = File.open("file.txt")
+#doc = REXML::Document.new file
+#file.close
+
 kittens = open("http://placekitten.com/")
 puts kittens.status #Prints the request status
 body = kittens.read[559, 441]
