0fc420a71b40911675ed2de0ec0ebdae6d93dec0
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-08-03 23:54:06 -0600

    Initializing a file that focuses on sorting. Adding notes on the combined comparison operator to the methods reference file

---
+++ Reference Material/sorting.rb
@@ -0,0 +1,9 @@
+#There are methods that can be called on arrays in order to sort the values by number (ascending values) or by letter (sorts strings/chars from a-z)
+
+my_array = [2, 16, 314, 4, 1 , 8, 92]
+
+puts my_array.sort!
+
+my_string_array = ["These","are","unsorted","strings"]
+
+puts my_string_array.sort!
