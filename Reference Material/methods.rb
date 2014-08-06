be481cee9199e07b9d5753a61a1ff5dd84fad3f3
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-08-05 23:33:42 -0600

    Created a basic function in ruby that demonstrates how functions are created and work with user input. Removed the comparison operator from the methods file

--- Reference Material/methods.rb
+++ Reference Material/methods.rb
@@ -1,14 +1,7 @@
-#This file will include methods/functions as well as other methods for comparison
+def userGreeting()
+	puts "Hello. What's your name?"
+	name = gets.chomp
+	return "Hello "+name
+end
 
-#Combined comparison
-#Using <=> in between two values of the same type (i.e. string vs string or int vs int) will return which value is greater or if they're equal. If the first value is greater, 1 is returned; 0 is for the values are equal; -1 if the second value is greater than the first
-
-number_1 = 2
-number_2 = 31
-
-puts number_1 <=> number_2
-
-string_1 = "Some string"
-string_2 = "Another string"
-
-puts string_1 <=> string_2
+userGreeting()
