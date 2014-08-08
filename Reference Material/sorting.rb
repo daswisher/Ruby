d588765031f3f39a001bfa6d128927f719cf24f6
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-08-07 20:36:18 -0600

    Added a quick way on how to sort an array and return the sort as a sorted descending order

--- Reference Material/sorting.rb
+++ Reference Material/sorting.rb
@@ -8,9 +8,17 @@ my_string_array = ["These","are","unsorted","strings"]
 
 puts my_string_array.sort!
 
+
 #There is the comparison that can be used to determine if something is less than numerically or alphabetically. If the first item is less than, it returns 0; Equal returns 0; Greater than the second item returns 1
 
 number_1 = 2
 number_2 = 31
 
 number_1<=>number_2 #This yields -1
+
+#It is possible to sort things in reverse order using the comparison operator and a loop
+fruits = ["orange", "apple", "banana", "pear", "grapes"]
+
+fruits.sort! do |firstBook, secondBoook|
+	secondBook <=> firstBook
+end
